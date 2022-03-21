504cf10c24bdcbe879f79c2561f15ef654511ac3
edsolater <edsolater@qq.com>
2022-03-21 18:16:21 +0800

    🐛 no drepacted raydium token list name

--- src/application/token/feature/useTokenListSettingsLocalStorage.tsx
+++ src/application/token/feature/useTokenListSettingsLocalStorage.tsx
@@ -2,7 +2,14 @@ import { useEffect } from 'react'
 
 import { getLocalItem, setLocalItem } from '@/functions/dom/jStorage'
 
-import { useToken } from '../useToken'
+import {
+  RAYDIUM_DEV_TOKEN_LIST_NAME,
+  RAYDIUM_MAINNET_TOKEN_LIST_NAME,
+  RAYDIUM_MAINNET_TOKEN_LIST_NAME_DEPRECATED,
+  SOLANA_TOKEN_LIST_NAME,
+  USER_ADDED_TOKEN_LIST_NAME,
+  useToken
+} from '../useToken'
 
 export default function useTokenListSettingsLocalStorage() {
   const tokenListSettings = useToken((s) => s.tokenListSettings)
@@ -12,8 +19,18 @@ export default function useTokenListSettingsLocalStorage() {
     const recordedTokenListSettings = getLocalItem('TOKEN_LIST_SETTINGS', (key, value) =>
       key === 'mints' ? new Set(value) : value
     )
+
     if (!recordedTokenListSettings) return
-    useToken.setState({ tokenListSettings: recordedTokenListSettings })
+    useToken.setState({
+      tokenListSettings: {
+        [RAYDIUM_MAINNET_TOKEN_LIST_NAME]:
+          recordedTokenListSettings[RAYDIUM_MAINNET_TOKEN_LIST_NAME] ??
+          recordedTokenListSettings[RAYDIUM_MAINNET_TOKEN_LIST_NAME_DEPRECATED],
+        [RAYDIUM_DEV_TOKEN_LIST_NAME]: recordedTokenListSettings[RAYDIUM_DEV_TOKEN_LIST_NAME],
+        [SOLANA_TOKEN_LIST_NAME]: recordedTokenListSettings[SOLANA_TOKEN_LIST_NAME],
+        [USER_ADDED_TOKEN_LIST_NAME]: recordedTokenListSettings[USER_ADDED_TOKEN_LIST_NAME]
+      }
+    })
   }, [])
 
   // whenever tokenListSettings changed, save it to localStorage
