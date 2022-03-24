4fca84431ac0cfa05e827f0f881a7c5574b58637
edsolater <edsolater@qq.com>
2022-03-24 16:41:24 +0800

    🚀 2.2.0

--- src/application/appVersion/useAppVersion.tsx
+++ src/application/appVersion/useAppVersion.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import create from 'zustand'
 
 // frontend (client)
-const APP_VERSION = 'V2.1.6'
+const APP_VERSION = 'V2.2.0'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
