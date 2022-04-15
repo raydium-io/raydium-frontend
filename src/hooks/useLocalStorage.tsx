b19e1b03d7f9793110fa61b42aa889422c68903a
edsolater <edsolater@qq.com>
2022-04-15 17:45:51 +0800

    🐛 react 18 think localstorage data should read in useEffect

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,18 +1,22 @@
-import { useCallback } from 'react'
+import { useCallback, useImperativeHandle } from 'react'
 
 import { getLocalItem, setLocalItem } from '@/functions/dom/jStorage'
 import { MayFunction } from '@/types/constants'
 
 import useXState from './useXState'
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect '
 
 /** auto json stringify and json parse */
 export default function useLocalStorageItem<T>(
   key: string
 ): [state: T | undefined, setState: (value: MayFunction<T, [old: T | undefined]>) => void] {
-  const [xStoredValue, setXStoredValue] = useXState<T | undefined>(
-    `(localStorage)${key}`,
-    () => getLocalItem(key) as any
-  )
+  const [xStoredValue, setXStoredValue] = useXState<T | undefined>(`(localStorage)${key}`)
+  useIsomorphicLayoutEffect(() => {
+    const storedValue = getLocalItem(key)
+    if (storedValue) {
+      setXStoredValue(storedValue)
+    }
+  }, [])
   const setValue = useCallback((value: MayFunction<T, [old: T | undefined]>) => {
     setXStoredValue(value)
     if (value) setLocalItem(key, value)
