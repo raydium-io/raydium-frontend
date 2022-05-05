a1077cce8a0ffcae2d153e66ebf977a133e40338
alpha.defi.9@gmail.com <alpha.defi.9@gmail.com>
2022-05-04 18:27:32 -0700

    🐛fix: broken link to acceleraytor on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -239,7 +239,7 @@ function HomePageSection1() {
             <Button
               className="frosted-glass-teal"
               onClick={() => {
-                push('/acceleraytor')
+                push('https://raydium.io/acceleraytor/')
               }}
             >
               View Projects
