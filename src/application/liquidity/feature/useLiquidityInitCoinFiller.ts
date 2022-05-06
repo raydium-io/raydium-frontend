bd1c388c2893566c16320367a803a726fecb7b01
edsolater <edsolater@qq.com>
2022-05-06 18:30:20 +0800

    🚑 v2.4.7 fix coin missing when pool  add liquidity

--- src/application/liquidity/feature/useLiquidityInitCoinFiller.ts
+++ src/application/liquidity/feature/useLiquidityInitCoinFiller.ts
@@ -10,12 +10,17 @@ import toPubString from '@/functions/format/toMintString'
 export default function useLiquidityInitCoinFiller() {
   const getToken = useToken((s) => s.getToken)
   useEffect(() => {
-    const { coin1, coin2 } = useLiquidity.getState()
-    if (!coin1 && toPubString(coin2?.mint) !== toPubString(QuantumSOLVersionSOL.mint)) {
-      useLiquidity.setState({ coin1: QuantumSOLVersionSOL })
-    }
-    if (!coin2 && toPubString(coin1?.mint) !== toPubString(RAYMint)) {
-      useLiquidity.setState({ coin2: getToken(RAYMint) })
-    }
+    setTimeout(() => {
+      // NOTE this effect must later than ammid parser
+      const { coin1, coin2, ammId } = useLiquidity.getState()
+      const isNotReady = Boolean(ammId && !coin1 && !coin2)
+      if (isNotReady) return
+      if (!coin1 && toPubString(coin2?.mint) !== toPubString(QuantumSOLVersionSOL.mint)) {
+        useLiquidity.setState({ coin1: QuantumSOLVersionSOL })
+      }
+      if (!coin2 && toPubString(coin1?.mint) !== toPubString(RAYMint)) {
+        useLiquidity.setState({ coin2: getToken(RAYMint) })
+      }
+    })
   }, [getToken])
 }
