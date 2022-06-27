3308987e2c5f4e4ab5ba115d3a8452baddbd0245
zhanggongying <edsolater@qq.com>
2022-06-27 15:23:29 +0800

    🚧 toBN round down

--- src/functions/numberish/toBN.ts
+++ src/functions/numberish/toBN.ts
@@ -4,6 +4,7 @@ import { Numberish } from '@/types/constants'
 import { BigNumberish, TEN } from '@raydium-io/raydium-sdk'
 
 import toFraction from './toFraction'
+import { shakeFractionDecimal } from './shakeFractionDecimal'
 
 /**
  * only int part will become BN
@@ -13,9 +14,5 @@ export default function toBN(n: Numberish, decimal?: BigNumberish): BN
 export default function toBN(n: Numberish | undefined, decimal: BigNumberish = 0): BN | undefined {
   if (!n) return undefined
   if (n instanceof BN) return n
-  return new BN(
-    toFraction(n)
-      .mul(TEN.pow(new BN(String(decimal))))
-      .toFixed(0)
-  )
+  return new BN(shakeFractionDecimal(toFraction(n).mul(TEN.pow(new BN(String(decimal))))))
 }
