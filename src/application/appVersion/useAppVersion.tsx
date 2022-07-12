af39c5038df6d294d10e1f0234bd769e63c1c3cc
zhanggongying <edsolater@qq.com>
2022-07-12 18:11:58 +0800

    ✨ add farm selector ; fix empty list item

--- src/application/appVersion/useAppVersion.tsx
+++ src/application/appVersion/useAppVersion.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import create from 'zustand'
 
 // frontend (client)
-const APP_VERSION = 'V2.6.5'
+const APP_VERSION = 'V2.6.6'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
