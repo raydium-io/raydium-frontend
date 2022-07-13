2335441da88106dcb472b3b98b38567adbe6ce2c
zhanggongying <edsolater@qq.com>
2022-07-13 13:18:59 +0800

    🚧 no fill when liquidity have url

--- src/application/liquidity/useLiquidityInitCoinFiller.tsx
+++ src/application/liquidity/useLiquidityInitCoinFiller.tsx
@@ -3,9 +3,10 @@ import { useEffect } from 'react'
 import useToken from '@/application/token/useToken'
 import { RAYMint } from '@/application/token/wellknownToken.config'
 
-import useLiquidity from './useLiquidity'
 import { QuantumSOLVersionSOL } from '@/application/token/quantumSOL'
+import { getURLQueryEntry } from '@/functions/dom/getURLQueryEntries'
 import toPubString from '@/functions/format/toMintString'
+import useLiquidity from './useLiquidity'
 
 export default function useLiquidityInitCoinFiller() {
   const getToken = useToken((s) => s.getToken)
@@ -13,12 +14,14 @@ export default function useLiquidityInitCoinFiller() {
     setTimeout(() => {
       // NOTE this effect must later than ammid parser
       const { coin1, coin2, ammId } = useLiquidity.getState()
+      const query = getURLQueryEntry()
       const isNotReady = Boolean(ammId && !coin1 && !coin2)
       if (isNotReady) return
-      if (!coin1 && toPubString(coin2?.mint) !== toPubString(QuantumSOLVersionSOL.mint)) {
+      const queryHaveSetCoin = ['coin0', 'coin1', 'ammId'].some((i) => Object.keys(query).includes(i))
+      if (!coin1 && toPubString(coin2?.mint) !== toPubString(QuantumSOLVersionSOL.mint) && !queryHaveSetCoin) {
         useLiquidity.setState({ coin1: QuantumSOLVersionSOL })
       }
-      if (!coin2 && toPubString(coin1?.mint) !== toPubString(RAYMint)) {
+      if (!coin2 && toPubString(coin1?.mint) !== toPubString(RAYMint) && !queryHaveSetCoin) {
         useLiquidity.setState({ coin2: getToken(RAYMint) })
       }
     })
--- src/application/swap/useSwapInitCoinFiller.tsx
+++ src/application/swap/useSwapInitCoinFiller.tsx
@@ -6,15 +6,18 @@ import { RAYMint } from '@/application/token/wellknownToken.config'
 import { useSwap } from './useSwap'
 import toPubString from '@/functions/format/toMintString'
 import { QuantumSOLVersionSOL } from '../token/quantumSOL'
+import { getURLQueryEntry } from '@/functions/dom/getURLQueryEntries'
 
 export default function useSwapInitCoinFiller() {
   const getToken = useToken((s) => s.getToken)
   useEffect(() => {
     const { coin1, coin2 } = useSwap.getState()
-    if (!coin1 && toPubString(coin2?.mint) !== toPubString(QuantumSOLVersionSOL.mint)) {
+    const query = getURLQueryEntry()
+    const queryHaveSetCoin = ['coin0', 'coin1', 'ammId'].some((i) => Object.keys(query).includes(i))
+    if (!coin1 && toPubString(coin2?.mint) !== toPubString(QuantumSOLVersionSOL.mint) && !queryHaveSetCoin) {
       useSwap.setState({ coin1: QuantumSOLVersionSOL })
     }
-    if (!coin2 && toPubString(coin1?.mint) !== toPubString(RAYMint)) {
+    if (!coin2 && toPubString(coin1?.mint) !== toPubString(RAYMint) && !queryHaveSetCoin) {
       useSwap.setState({ coin2: getToken(RAYMint) })
     }
   }, [getToken])
