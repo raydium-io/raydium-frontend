40c6edf3e9f8c88c50042791fae048cf6f2967ae
zhanggongying <edsolater@qq.com>
2022-07-25 12:16:45 +0800

    🐛 revert `<FadeIn>` change

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -5,11 +5,13 @@ import useToggle from '@/hooks/useToggle'
 import { Transition } from '@headlessui/react'
 
 export default function FadeInStable({
+  openDelay = 17,
   ignoreEnterTransition,
   ignoreLeaveTransition,
   show,
   children
 }: {
+  openDelay?: number
   ignoreEnterTransition?: boolean
   ignoreLeaveTransition?: boolean
   show?: any
@@ -21,13 +23,11 @@ export default function FadeInStable({
   return (
     <Transition
       show={Boolean(show)}
-      appear
-      static
       // unmount={false} // TODO: although it will lose state, but not unmount will lose element's height, cause display: none will make height lose. so, have to customized a <Transition> component.
-      enter="select-none transition-all duration-1000 ease"
+      enter="select-none transition-all duration-200 ease"
       enterFrom="opacity-0"
       enterTo="opacity-100"
-      leave="select-none transition-all duration-1000 ease"
+      leave="select-none transition-all duration-200 ease"
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
       beforeEnter={() => {
@@ -35,29 +35,20 @@ export default function FadeInStable({
         // seems headlessui/react 1.6 will get react 18's priority strategy. 👇 fllowing code will invoke **before** element load
         contentRef.current?.style.setProperty('position', 'absolute') // init will rerender element, "position:absolute" is for not affect others
         contentRef.current?.style.setProperty('visibility', 'hidden')
-        // get a layout property to manually to force the browser to layout the above code.
-        // So trick. But have to.🤯🤯🤯🤯
-        contentRef.current?.clientHeight
-        contentRef.current?.style.removeProperty('position')
-        contentRef.current?.style.removeProperty('visibility')
+        setTimeout(() => {
+          contentRef.current?.style.removeProperty('position')
+          contentRef.current?.style.removeProperty('visibility')
 
-        const height = contentRef.current?.clientHeight
-        // frequent ui action may cause element havn't attach to DOM yet, when occors, just ignore it.
-        if (height) {
-          contentRef.current?.style.setProperty('height', '0')
-          // get a layout property to manually to force the browser to layout the above code.
-          // So trick. But have to.🤯🤯🤯🤯
-          const AFid = window.requestAnimationFrame(() => {
-            const t = contentRef.current?.clientHeight
+          const height = contentRef.current?.clientHeight
+          // frequent ui action may cause element havn't attach to DOM yet, when occors, just ignore it.
+          if (height) {
+            contentRef.current?.style.setProperty('height', '0')
+            // get a layout property to manually to force the browser to layout the above code.
+            // So trick. But have to.🤯🤯🤯🤯
+            contentRef.current?.clientHeight
             contentRef.current?.style.setProperty('height', `${height}px`)
-            contentRef.current?.addEventListener('transitionend', () => {})
-            window.cancelAnimationFrame(AFid)
-          })
-          // setTimeout(() => {
-          //   const t = contentRef.current?.clientHeight
-          //   // contentRef.current?.style.setProperty('height', `${height}px`)
-          // })
-        }
+          }
+        }, openDelay)
         turnOnDuringTransition()
       }}
       afterEnter={() => {
@@ -66,19 +57,19 @@ export default function FadeInStable({
       }}
       beforeLeave={() => {
         if (ignoreLeaveTransition) return
-        contentRef.current?.style.setProperty('position', 'absolute')
-        contentRef.current?.style.setProperty('visibility', 'hidden')
+        setTimeout(() => {
+          const height = contentRef.current?.clientHeight
+          if (!height) {
+            contentRef.current?.style.setProperty('height', '0')
+          } else {
+            contentRef.current?.style.setProperty('height', `${height}px`)
+            // get a layout property to manually to force the browser to layout the above code.
+            // So trick. But have to.🤯🤯🤯🤯
+            contentRef.current?.clientHeight
+            contentRef.current?.style.setProperty('height', '0')
+          }
+        })
         turnOnDuringTransition()
-        const height = contentRef.current?.clientHeight
-        if (!height) {
-          contentRef.current?.style.setProperty('height', '0')
-        } else {
-          contentRef.current?.style.setProperty('height', `${height}px`)
-          // get a layout property to manually to force the browser to layout the above code.
-          // So trick. But have to.🤯🤯🤯🤯
-          contentRef.current?.clientHeight
-          contentRef.current?.style.setProperty('height', '0')
-        }
       }}
       afterLeave={() => {
         contentRef.current?.style.removeProperty('height')
@@ -90,9 +81,8 @@ export default function FadeInStable({
       {/* outer div can't set ref for it's being used by headless-ui <Transition/> */}
       <div
         ref={contentRef}
-        style={{ position: 'absolute', visibility: 'hidden', transition: '1000ms' }}
         className={twMerge(
-          `transition-all duration-1000 ease overflow-hidden ${children || isDuringTransition ? '' : 'hidden'}`
+          `transition-all duration-200 ease overflow-hidden ${children || isDuringTransition ? '' : 'hidden'}`
         )}
       >
         {children}
@@ -102,11 +92,14 @@ export default function FadeInStable({
 }
 
 export function FadeIn({
+  openDelay = 35,
   ignoreEnterTransition,
   ignoreLeaveTransition,
   className,
   children
 }: {
+  // if immediately, inner content maybe be still not render ready
+  openDelay?: number
   ignoreEnterTransition?: boolean
   ignoreLeaveTransition?: boolean
   className?: string
@@ -130,23 +123,22 @@ export function FadeIn({
       beforeEnter={() => {
         if (ignoreEnterTransition) return
         // seems headlessui/react 1.6 will get react 18's priority strategy. 👇 fllowing code will invoke **before** element load
+        contentRef.current?.style.setProperty('position', 'absolute') // init will rerender element, "position:absolute" is for not affect others
+        contentRef.current?.style.setProperty('visibility', 'hidden')
+        setTimeout(() => {
+          contentRef.current?.style.removeProperty('position')
+          contentRef.current?.style.removeProperty('visibility')
 
-        contentRef.current?.style.removeProperty('position')
-        contentRef.current?.style.removeProperty('visibility')
-
-        const height = contentRef.current?.clientHeight
-        // frequent ui action may cause element havn't attach to DOM yet, when occors, just ignore it.
-        if (height) {
-          contentRef.current?.style.setProperty('height', '0')
-          // get a layout property to manually to force the browser to layout the above code.
-          // So trick. But have to.🤯🤯🤯🤯
-          const AFid = window.requestAnimationFrame(() => {
-            const t = contentRef.current?.clientHeight
+          const height = contentRef.current?.clientHeight
+          // frequent ui action may cause element havn't attach to DOM yet, when occors, just ignore it.
+          if (height) {
+            contentRef.current?.style.setProperty('height', '0')
+            // get a layout property to manually to force the browser to layout the above code.
+            // So trick. But have to.🤯🤯🤯🤯
+            contentRef.current?.clientHeight
             contentRef.current?.style.setProperty('height', `${height}px`)
-            contentRef.current?.addEventListener('transitionend', () => {})
-            window.cancelAnimationFrame(AFid)
-          })
-        }
+          }
+        }, openDelay)
         turnOnDuringTransition()
       }}
       afterEnter={() => {
@@ -176,7 +168,6 @@ export function FadeIn({
     >
       <div
         ref={contentRef}
-        style={{ position: 'absolute', visibility: 'hidden', transition: '1000ms' }}
         className={`transition-all duration-200 ease overflow-hidden ${children || isDuringTransition ? '' : 'hidden'}`}
       >
         {innerChildren.current}
