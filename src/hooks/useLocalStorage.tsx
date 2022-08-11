c5f4743eacca0b6a79b0a39c5490a9413d021098
zhanggongying <edsolater@qq.com>
2022-08-11 16:46:55 +0800

    🐛 fix favourite system 2

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -7,18 +7,20 @@ import useXState from './useXState'
 import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect '
 
 /** auto json stringify and json parse */
-export default function useLocalStorageItem<T>(
+export default function useLocalStorageItem<T, E = T>(
   key: string,
   options?: {
     defaultValue?: T
+    emptyValue?: E
     validateFn?: (value: T | undefined) => boolean
   }
-): [state: T | undefined, setState: (value: MayFunction<T, [old: T | undefined]>) => void] {
-  const [xStoredValue, setXStoredValue] = useXState<T | undefined>(`${key}`, options?.defaultValue)
+): [state: T | E | undefined, setState: (value: MayFunction<T, [old: T | undefined]>) => void] {
+  const { defaultValue, emptyValue = defaultValue, validateFn } = options ?? {}
+  const [xStoredValue, setXStoredValue] = useXState<T | undefined>(`${key}`, defaultValue)
   useIsomorphicLayoutEffect(() => {
     const storedValue = getLocalItem(key)
-    if (options?.validateFn?.(storedValue) ?? true) {
-      setXStoredValue(storedValue)
+    if (validateFn?.(storedValue) ?? true) {
+      setXStoredValue(storedValue ?? emptyValue)
     }
   }, [])
   const setValue = useCallback((value: MayFunction<T, [old: T | undefined]>) => {
