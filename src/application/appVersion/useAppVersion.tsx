6247748037408abd38eb39efe24feedd38306e94
zhanggongying <edsolater@qq.com>
2022-08-12 19:32:13 +0800

    🚀 V2.7.7 migration tool

--- src/application/appVersion/useAppVersion.tsx
+++ src/application/appVersion/useAppVersion.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import create from 'zustand'
 
 // frontend (client)
-const APP_VERSION = 'V2.7.6'
+const APP_VERSION = 'V2.7.7'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
