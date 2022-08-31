f3d2c2876f661da453441c0ad9d400d5eb518f90
Jack <jack77121@gmail.com>
2022-08-31 17:37:18 +0800

    ✨ add unknown token supports from Pools to Swap/Liquidity

--- src/application/token/useToken.tsx
+++ src/application/token/useToken.tsx
@@ -1,27 +1,20 @@
 import { Price, PublicKeyish } from '@raydium-io/raydium-sdk'
 
+import produce from 'immer'
 import create from 'zustand'
 
 import { addItem, removeItem, shakeUndifindedItem } from '@/functions/arrayMethods'
+import toPubString from '@/functions/format/toMintString'
 import { HexAddress, SrcAddress } from '@/types/constants'
 
 import useWallet from '../wallet/useWallet'
 
-import { LpToken, SplToken, TokenJson } from './type'
 import {
-  isQuantumSOL,
-  isQuantumSOLVersionSOL,
-  isQuantumSOLVersionWSOL,
-  QuantumSOLToken,
-  QuantumSOLVersionSOL,
-  QuantumSOLVersionWSOL,
-  SOLUrlMint,
-  WSOLMint
+  isQuantumSOL, isQuantumSOLVersionSOL, isQuantumSOLVersionWSOL, QuantumSOLToken, QuantumSOLVersionSOL,
+  QuantumSOLVersionWSOL, SOLUrlMint, WSOLMint
 } from './quantumSOL'
+import { LpToken, SplToken, TokenJson } from './type'
 import { RAYMint } from './wellknownToken.config'
-import toPubString from '@/functions/format/toMintString'
-import { objectShakeNil } from '@/functions/objectMethods'
-import produce from 'immer'
 
 export type TokenStore = {
   tokenIconSrcs: Record<HexAddress, SrcAddress>
@@ -98,11 +91,13 @@ export type SupportedTokenListSettingName =
   | 'Raydium Dev Token List'
   | 'Solana Token List' // actually  unOfficial
   | 'User Added Token List'
+  | 'Other Liquidity Supported Token List' // other tokens in liquidity pools
 export const RAYDIUM_MAINNET_TOKEN_LIST_NAME_DEPRECATED = 'Raydium Mainnet Token List'
 export const RAYDIUM_MAINNET_TOKEN_LIST_NAME = 'Raydium Token List'
 export const RAYDIUM_DEV_TOKEN_LIST_NAME = 'Raydium Dev Token List'
 export const SOLANA_TOKEN_LIST_NAME = 'Solana Token List'
 export const USER_ADDED_TOKEN_LIST_NAME = 'User Added Token List'
+export const OTHER_LIQUIDITY_SUPPORTED_TOKEN_LIST_NAME = 'Other Liquidity Supported Token List'
 
 /** zustand store hooks */
 export const useToken = create<TokenStore>((set, get) => ({
@@ -214,6 +209,9 @@ export const useToken = create<TokenStore>((set, get) => ({
       isOn: true
     },
     [USER_ADDED_TOKEN_LIST_NAME]: {
+      isOn: true
+    },
+    [OTHER_LIQUIDITY_SUPPORTED_TOKEN_LIST_NAME]: {
       isOn: true
     }
   },
