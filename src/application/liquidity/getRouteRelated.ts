004490112f0c8fa542747b78764a3f2548706cbf
zhanggongying <edsolater@qq.com>
2022-09-05 16:36:27 +0800

    🚑 refresh swap SWOLE simulate once

---
+++ src/application/liquidity/getRouteRelated.ts
@@ -0,0 +1,59 @@
+import { LiquidityPoolJsonInfo as LiquidityJsonInfo, PublicKeyish } from '@raydium-io/raydium-sdk'
+import toPubString from '@/functions/format/toMintString'
+import { WSOLMint } from '../token/quantumSOL'
+import {
+  ETHMint,
+  mSOLMint,
+  PAIMint,
+  RAYMint,
+  stSOLMint,
+  USDCMint,
+  USDHMint,
+  USDTMint
+} from '../token/wellknownToken.config'
+
+/**
+ * is A - Middl - B
+ */
+export function getRouteRelated(
+  jsonInfos: LiquidityJsonInfo[],
+  targetMint1: PublicKeyish,
+  targetMint2: PublicKeyish
+): LiquidityJsonInfo[] {
+  const routeMiddleMints = [USDCMint, RAYMint, WSOLMint, mSOLMint, PAIMint, stSOLMint, USDHMint, USDTMint, ETHMint].map(
+    toPubString
+  )
+  const mintA = toPubString(targetMint1)
+  const mintB = toPubString(targetMint2)
+
+  const relatedA = jsonInfos.filter(
+    (i) =>
+      (i.baseMint === mintA && routeMiddleMints.includes(i.quoteMint)) ||
+      (i.quoteMint === mintA && routeMiddleMints.includes(i.baseMint))
+  )
+  const relatedB = jsonInfos.filter(
+    (i) =>
+      (i.baseMint === mintB && routeMiddleMints.includes(i.quoteMint)) ||
+      (i.quoteMint === mintB && routeMiddleMints.includes(i.baseMint))
+  )
+  const mintDirectComposeIsValid = (mintA1: string, mintA2: string, mintB1: string, mintB2: string) =>
+    Boolean(mintA1 === mintA && mintA2 == mintB1 && mintB2 === mintB)
+
+  const isValidRoutePair = (a: LiquidityJsonInfo, b: LiquidityJsonInfo) =>
+    mintDirectComposeIsValid(a.baseMint, a.quoteMint, b.baseMint, b.quoteMint) ||
+    mintDirectComposeIsValid(a.baseMint, a.quoteMint, b.quoteMint, b.baseMint) ||
+    mintDirectComposeIsValid(a.quoteMint, a.baseMint, b.baseMint, b.quoteMint) ||
+    mintDirectComposeIsValid(a.quoteMint, a.baseMint, b.quoteMint, b.baseMint)
+  const onlyRoutes = new Set<LiquidityJsonInfo>()
+
+  for (const relA of relatedA) {
+    for (const relB of relatedB) {
+      if (isValidRoutePair(relA, relB)) {
+        onlyRoutes.add(relA)
+        onlyRoutes.add(relB)
+      }
+    }
+  }
+
+  return [...onlyRoutes]
+}
--- src/application/liquidity/useLiquidity.tsx
+++ src/application/liquidity/useLiquidity.tsx
@@ -2,25 +2,16 @@ import { LiquidityPoolJsonInfo as LiquidityJsonInfo, PublicKeyish } from '@raydi
 
 import create from 'zustand'
 
-import toPubString from '@/functions/format/toMintString'
+import toPubString, { toPub } from '@/functions/format/toMintString'
 import { gte } from '@/functions/numberish/compare'
 import { div } from '@/functions/numberish/operations'
 
-import { toDataMint, WSOLMint } from '../token/quantumSOL'
+import { toDataMint } from '../token/quantumSOL'
 import { SplToken } from '../token/type'
-import {
-  ETHMint,
-  mSOLMint,
-  PAIMint,
-  RAYMint,
-  stSOLMint,
-  USDCMint,
-  USDHMint,
-  USDTMint
-} from '../token/wellknownToken.config'
 
 import sdkParseJsonLiquidityInfo from './sdkParseJsonLiquidityInfo'
 import { HydratedLiquidityInfo, SDKParsedLiquidityInfo } from './type'
+import { getRouteRelated } from './getRouteRelated'
 
 export type LiquidityStore = {
   // too tedius
@@ -129,24 +120,8 @@ const useLiquidity = create<LiquidityStore>((set, get) => ({
     )
 
     /** swap's route transaction middle token  */
-    const routeMiddleMints = [
-      USDCMint,
-      RAYMint,
-      WSOLMint,
-      mSOLMint,
-      PAIMint,
-      stSOLMint,
-      USDHMint,
-      USDTMint,
-      ETHMint
-    ].map(toPubString)
-    const candidateTokenMints = routeMiddleMints.concat([mint1, mint2])
-    const onlyRouteMints = routeMiddleMints.filter((routeMint) => ![mint1, mint2].includes(routeMint))
-    const routeRelated = get().jsonInfos.filter((info) => {
-      const isCandidate = candidateTokenMints.includes(info.baseMint) && candidateTokenMints.includes(info.quoteMint)
-      const onlyInRoute = onlyRouteMints.includes(info.baseMint) && onlyRouteMints.includes(info.quoteMint)
-      return isCandidate && !onlyInRoute
-    })
+    const onlyRoutes = getRouteRelated(get().jsonInfos, mint1, mint2)
+    const routeRelated = availables.concat(onlyRoutes)
 
     const best = await (async () => {
       if (availables.length === 0) return undefined
