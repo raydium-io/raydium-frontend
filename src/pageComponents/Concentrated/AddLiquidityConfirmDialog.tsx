c2047bd896ef415c33fbba2d05285e7b7669c430
CruzShia <iamahan0720@gmail.com>
2022-09-29 19:39:01 +0800

    add back button

--- src/pageComponents/Concentrated/AddLiquidityConfirmDialog.tsx
+++ src/pageComponents/Concentrated/AddLiquidityConfirmDialog.tsx
@@ -64,6 +64,7 @@ export default function AddLiquidityConfirmDialog({
           size="lg"
         >
           <Row className="justify-between items-center mb-6">
+            <Icon className="text-[#ABC4FF] cursor-pointer" heroIconName="chevron-left" onClick={close} />
             <div className="text-xl font-semibold text-white">
               Preview Deposit to {coin1?.symbol} / {coin2?.symbol}
             </div>
