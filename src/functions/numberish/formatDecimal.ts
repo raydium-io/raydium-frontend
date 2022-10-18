1febdf7a76a8a51f51c118b90be4e6c3505df558
CruzShia <iamahan0720@gmail.com>
2022-10-18 16:46:34 +0800

    adjust int digit

--- src/functions/numberish/formatDecimal.ts
+++ src/functions/numberish/formatDecimal.ts
@@ -9,7 +9,7 @@ export const formatDecimal = ({
 }): number => {
   const valStr = String(val)
   const valArr = valStr.split('.')
-  const [intDigit, decimalLength] = [(valArr[0]?.length || 1) - 1, valArr[1]?.length || 0]
+  const [intDigit, decimalLength] = [valArr[0]?.length || 1, valArr[1]?.length || 0]
   if (decimalLength <= decimals) return Number(val)
   return Number(Number(val).toFixed(Math.max(maxLength - intDigit, decimals)))
 }
