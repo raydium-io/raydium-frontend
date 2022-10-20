e4899d0f084fb671ff98552cb24181cacdcd3dba
zhanggongying <edsolater@qq.com>
2022-10-20 18:25:18 +0800

    🔥 clean death code

--- src/pages/clmm/create-pool.tsx
+++ src/pages/clmm/create-pool.tsx
@@ -1,43 +1,22 @@
 import useAppSettings from '@/application/common/useAppSettings'
-import txCreateNewConcentratedPool from '@/application/concentrated/txCreateNewConcentratedPool'
-import useConcentrated from '@/application/concentrated/useConcentrated'
 import useConcentratedAmmConfigInfoLoader from '@/application/concentrated/useConcentratedAmmConfigInfoLoader'
 import useConcentratedAmountCalculator from '@/application/concentrated/useConcentratedAmountCalculator'
-import useNotification from '@/application/notification/useNotification'
 import { routeBack, routeTo } from '@/application/routeTools'
-import useWallet from '@/application/wallet/useWallet'
 import Button from '@/components/Button'
 import Card from '@/components/Card'
 import { Checkbox } from '@/components/Checkbox'
 import Col from '@/components/Col'
-import CyberpunkStyleCard from '@/components/CyberpunkStyleCard'
 import FadeInStable from '@/components/FadeIn'
 import Icon from '@/components/Icon'
 import PageLayout from '@/components/PageLayout'
 import Row from '@/components/Row'
 import { getLocalItem, setLocalItem } from '@/functions/dom/jStorage'
-import { toTokenAmount } from '@/functions/format/toTokenAmount'
-import toUsdVolume from '@/functions/format/toUsdVolume'
-import { isMeaningfulNumber } from '@/functions/numberish/compare'
-import toFraction from '@/functions/numberish/toFraction'
 import { useEvent } from '@/hooks/useEvent'
-import useToggle from '@/hooks/useToggle'
 import { CreatePoolCard } from '@/pageComponents/createConcentratedPool/CreatePoolCard'
-import CreatePoolPreviewDialog from '@/pageComponents/createConcentratedPool/CreatePoolPreviewDialog'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 // unless ido have move this component, it can't be renamed or move to /components
-function StepBadge(props: { n: number }) {
-  return (
-    <CyberpunkStyleCard
-      wrapperClassName="w-8 h-8 mobile:w-6 mobile:h-6 flex-none"
-      className="grid place-content-center bg-[#2f2c78]"
-    >
-      <div className="font-semibold text-white mobile:text-xs">{props.n}</div>
-    </CyberpunkStyleCard>
-  )
-}
 
 function NavButtons({ className }: { className?: string }) {
   return (
@@ -115,62 +94,6 @@ export default function CreatePoolPage() {
   useConcentratedAmountCalculator()
   const isMobile = useAppSettings((s) => s.isMobile)
 
-  const { popConfirm } = useNotification()
-
-  // avoid input re-render if chain Date change
-
-  const coin1 = useConcentrated((s) => s.coin1)
-  const coin1Amount = useConcentrated((s) => s.coin1Amount)
-  const coin2 = useConcentrated((s) => s.coin2)
-  const coin2Amount = useConcentrated((s) => s.coin2Amount)
-  const focusSide = useConcentrated((s) => s.focusSide)
-  const totalDeposit = useConcentrated((s) => s.totalDeposit)
-  const userSettedCurrentPrice = useConcentrated((s) => s.userSettedCurrentPrice)
-  const priceLower = useConcentrated((s) => s.priceLower)
-  const priceUpper = useConcentrated((s) => s.priceUpper)
-
-  const decimals = coin1 || coin2 ? Math.max(coin1?.decimals ?? 0, coin2?.decimals ?? 0) : 6
-
-  function popCongratulations() {
-    popConfirm({
-      type: 'success',
-      title: 'Pool created successfully!',
-      description: 'Do you want to create a farm based on this pool?',
-      confirmButtonIsMainButton: true,
-      confirmButtonText: 'Back to all Pools',
-      cancelButtonText: 'Not Now',
-      onConfirm() {
-        routeTo('/clmm/pools')
-        setTimeout(() => {
-          // clean inputs
-          useConcentrated.setState({
-            coin1: undefined,
-            coin2: undefined,
-            coin1Amount: undefined,
-            coin2Amount: undefined,
-            focusSide: 'coin1',
-            userCursorSide: 'coin1',
-            tempDataCache: undefined
-          })
-          useConcentrated.getState().refreshConcentrated()
-        }, 400)
-      },
-      onCancel() {
-        setTimeout(() => {
-          // clean inputs
-          useConcentrated.setState({
-            coin1: undefined,
-            coin2: undefined,
-            coin1Amount: undefined,
-            coin2Amount: undefined,
-            focusSide: 'coin1',
-            userCursorSide: 'coin1'
-          })
-          useConcentrated.getState().refreshConcentrated()
-        }, 400)
-      }
-    })
-  }
   return (
     <PageLayout metaTitle="Farms - Raydium" mobileBarTitle="Create Farm">
       <NavButtons className="mb-8 mobile:mb-2 sticky z-10 top-0 mobile:-translate-y-2 mobile:bg-[#0f0b2f] mobile:hidden" />
