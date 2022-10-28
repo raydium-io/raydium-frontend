8b4a8f37761ff88094f866484a8cb2234fae7a76
zhanggongying <edsolater@qq.com>
2022-10-28 15:40:09 +0800

    💄 prettier tx notification

--- src/components/NotificationItem/NormalNotificationItemCard.tsx
+++ src/components/NotificationItem/NormalNotificationItemCard.tsx
@@ -7,7 +7,7 @@ import Row from '../Row'
 import { spawnTimeoutControllers } from './utils'
 import { NormalNotificationItemInfo } from './type'
 
-const existMs = process.env.NODE_ENV === 'development' ? 10 * 60 * 1000 : 4 * 1000 // (ms)
+const existMs = process.env.NODE_ENV === 'development' ? 2 * 60 * 1000 : 4 * 1000 // (ms)
 
 const colors: Record<
   NormalNotificationItemInfo['type'] & string,
--- src/components/NotificationItem/TxNotificationItemCard.tsx
+++ src/components/NotificationItem/TxNotificationItemCard.tsx
@@ -1,4 +1,5 @@
 import useAppSettings from '@/application/common/useAppSettings'
+import { useEvent } from '@/hooks/useEvent'
 import { useHover } from '@/hooks/useHover'
 import { useSignalState } from '@/hooks/useSignalState'
 import useToggle from '@/hooks/useToggle'
@@ -13,7 +14,7 @@ import Row from '../Row'
 import { TxNotificationController, TxNotificationItemInfo } from './type'
 import { spawnTimeoutControllers } from './utils'
 
-const existMs = process.env.NODE_ENV === 'development' ? 1 * 60 * 1000 : 1 * 60 * 1000 // (ms)
+const existMs = process.env.NODE_ENV === 'development' ? 2 * 60 * 1000 : 6 * 1000 // (ms)
 
 const colors = {
   success: {
@@ -40,7 +41,7 @@ const colors = {
     text: 'text-[#D8CB39]',
     bg: 'bg-[#D8CB39]'
   }
-}
+} as const
 
 export function TxNotificationItemCard({
   info: { txInfos },
@@ -55,15 +56,24 @@ export function TxNotificationItemCard({
   const explorerName = useAppSettings((s) => s.explorerName)
   // cache for componentRef to change it
   const [innerTxInfos, setInnerTxInfos, innerTxInfosSignal] = useSignalState(txInfos)
-  const [, setTxAllProcessed, txAllProcessedSignal] = useSignalState(false)
+  const wholeItemState = innerTxInfos.every(({ state }) => state === 'success')
+    ? 'success'
+    : innerTxInfos.some(({ state }) => state === 'error')
+    ? 'error'
+    : 'info'
+  const totalTransactionLength = innerTxInfos.length
+  const processedTransactionLength = innerTxInfos.filter(({ state }) => state === 'success' || state === 'error').length
+  const isAllProcessed = useEvent(() => {
+    const txAllProcessed = processedTransactionLength === totalTransactionLength
+    return txAllProcessed
+  })
+
   useEffect(() => {
-    const txAllProcessed = innerTxInfos.every(({ state }) => state && state !== 'processing' && state !== 'queuing')
-    if (txAllProcessed) {
-      setTxAllProcessed(txAllProcessed)
+    if (wholeItemState === 'success') {
       timeoutController.current.start()
       resumeTimeline()
     }
-  }, [innerTxInfos])
+  }, [wholeItemState])
 
   const [isTimePassing, { off: pauseTimeline, on: resumeTimeline }] = useToggle(false)
 
@@ -78,7 +88,7 @@ export function TxNotificationItemCard({
 
   useHover(itemRef, {
     onHover({ is: now }) {
-      if (!txAllProcessedSignal()) return
+      if (!isAllProcessed()) return
       if (now === 'start') {
         timeoutController.current.pause()
         pauseTimeline()
@@ -108,15 +118,10 @@ export function TxNotificationItemCard({
       } as TxNotificationController)
   )
 
-  const wholeItemState = innerTxInfos.every(({ state }) => state === 'success')
-    ? 'success'
-    : innerTxInfos.some(({ state }) => state === 'error')
-    ? 'error'
-    : 'info'
   return (
     <Card
       domRef={itemRef}
-      className={`min-w-[260px] relative rounded-xl ring-1.5 ring-inset ${colors[wholeItemState].ring} bg-[#1B1659] py-4 pl-5 pr-10 mx-4 my-2 overflow-hidden pointer-events-auto transition`}
+      className={`min-w-[260px] relative rounded-xl ring-1.5 ring-inset ${colors[wholeItemState].ring} bg-[#1B1659] p-6 mx-4 my-2 overflow-hidden pointer-events-auto transition`}
     >
       {/* timeline */}
       <div className="h-1 absolute top-0 left-0 right-0">
@@ -124,7 +129,7 @@ export function TxNotificationItemCard({
         <div className={`opacity-5 ${colors[wholeItemState].bg} absolute inset-0 transition`} />
         {/* remain-line */}
         <div
-          className={`${colors[wholeItemState].bg} absolute inset-0`}
+          className={`${colors[wholeItemState].bg} absolute inset-0 py-`}
           style={{
             animation: `shrink ${existMs}ms linear forwards`,
             animationPlayState: isTimePassing ? 'running' : 'paused'
@@ -135,7 +140,7 @@ export function TxNotificationItemCard({
       <Icon
         size="sm"
         heroIconName="x"
-        className="absolute right-3 top-3 clickable text-[rgba(171,196,255,0.5)]"
+        className="absolute right-6 top-6 clickable text-[#abc4ff]"
         onClick={() => {
           timeoutController.current.cancel()
           close()
@@ -143,45 +148,55 @@ export function TxNotificationItemCard({
       />
       <Row className="gap-3 px-2 mobile:px-0">
         <div>
-          <div className="mb-2">
-            <div className="font-medium text-lg  mobile:p-0 mobile:text-sm text-white">
-              {innerTxInfos[0].historyInfo.title}
+          <Row className="gap-3 mb-5">
+            <Icon heroIconName={colors[wholeItemState].heroIconName} className={colors[wholeItemState].text} />
+            <div>
+              <div className="font-medium text-base mobile:p-0 mobile:text-sm text-white">
+                {wholeItemState === 'success'
+                  ? `${innerTxInfos[0].historyInfo.title} Confirmed!`
+                  : wholeItemState === 'error'
+                  ? `${innerTxInfos[0].historyInfo.title} Error!`
+                  : `Confirming transaction...(${processedTransactionLength + 1}/${totalTransactionLength})`}
+              </div>
+              <div className="font-medium text-sm mobile:text-sm text-[rgba(171,196,255,0.5)] mt-1">
+                {innerTxInfos[0].historyInfo.description}
+              </div>
             </div>
-            <div className="font-medium text-sm mobile:text-sm text-[rgba(171,196,255,0.5)]">
-              {innerTxInfos[0].historyInfo.description}
-            </div>
-          </div>
-          <Col className="gap-3 mobile:gap-2">
+          </Row>
+          <Col className="gap-2 mobile:gap-2 max-h-[252px] mobile:max-h-[140px] overflow-y-auto px-2 -mx-2 ">
             {innerTxInfos.map(({ state, txid }, idx) => (
-              <Row key={idx} className="items-center gap-2 mobile:gap-1">
-                <div>
-                  {state === 'processing' ? (
-                    <LoadingCircleSmall className="h-6 w-6 mobile:h-4 mobile:w-4 scale-75" />
-                  ) : state === 'success' ? (
-                    <Icon heroIconName="check-circle" size={isMobile ? 'sm' : 'md'} className="text-[#39d0d8]" />
-                  ) : state === 'error' ? (
-                    <Icon heroIconName="exclamation-circle" size={isMobile ? 'sm' : 'md'} className="text-[#DA2EEF]" />
-                  ) : (
-                    <div className="h-6 w-6 mobile:h-4 mobile:w-4"></div>
-                  )}
-                </div>
+              <Row key={idx} className="justify-between items-center p-3 gap-20 mobile:gap-1 bg-[#141041] rounded-lg">
+                <Row className="gap-1.5">
+                  {/* item icon */}
+                  <div>
+                    {state === 'success' ? (
+                      <Icon heroIconName="check-circle" size={isMobile ? 'sm' : 'smi'} className="text-[#39d0d8]" />
+                    ) : state === 'error' ? (
+                      <Icon
+                        heroIconName="exclamation-circle"
+                        size={isMobile ? 'sm' : 'smi'}
+                        className="text-[#DA2EEF]"
+                      />
+                    ) : (
+                      <LoadingCircleSmall className="h-5 w-5 mobile:h-4 mobile:w-4 scale-75" />
+                    )}
+                  </div>
 
-                <Row className="text-sm mobile:text-xs text-[#abc4ff80] gap-2">
-                  {innerTxInfos.length > 1 ? (
-                    <>
-                      <div>Step {idx + 1}</div>
-                      {txid && (
-                        <>
-                          <div className="opacity-50">{'|'}</div>
-                          <div>
-                            View on <LinkExplorer hrefDetail={`tx/${txid}`}>{explorerName}</LinkExplorer>
-                          </div>
-                        </>
-                      )}
-                    </>
+                  {/* item text */}
+                  <div className="text-sm mobile:text-xs font-medium text-[#abc4ff]">Transaction {idx + 1}</div>
+                </Row>
+
+                <Row className="text-sm mobile:text-xs text-[#abc4ff] gap-2">
+                  {txid ? (
+                    <Row className="items-center gap-1.5 opacity-50 mobile:opacity-100 hover:opacity-100 transition-opacity">
+                      <div>
+                        View on <LinkExplorer hrefDetail={`tx/${txid}`}>{explorerName}</LinkExplorer>
+                      </div>
+                      <Icon heroIconName="external-link" size="xs" className="text-[#abc4ff]" />
+                    </Row>
                   ) : (
-                    <div>
-                      View on <LinkExplorer hrefDetail={`tx/${txid}`}>{explorerName}</LinkExplorer>
+                    <div className="text-[#abc4ff] text-xs opacity-50 mobile:opacity-100 transition-opacity">
+                      Waiting...
                     </div>
                   )}
                 </Row>
