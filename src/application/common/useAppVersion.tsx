5e0826e0d7f32447366ed8df26a83cc75e3429f7
zhanggongying <edsolater@qq.com>
2022-11-04 14:11:23 +0800

    🚀 V2.8.26

--- src/application/common/useAppVersion.tsx
+++ src/application/common/useAppVersion.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import create from 'zustand'
 
 // frontend (client)
-const APP_VERSION = 'V2.8.25'
+const APP_VERSION = 'V2.8.26'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
