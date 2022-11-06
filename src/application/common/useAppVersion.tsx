c0738efbefd1e63fa37c062aea129be2feb98c0a
edsolater <edsolater@qq.com>
2022-11-06 11:37:40 +0800

    🚑🐛 fix liquidity current sdkParsed bug (but don't know why)

--- src/application/common/useAppVersion.tsx
+++ src/application/common/useAppVersion.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import create from 'zustand'
 
 // frontend (client)
-const APP_VERSION = 'V2.8.27'
+const APP_VERSION = 'V2.8.28'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
