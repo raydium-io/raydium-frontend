ebe4d295a9ea7c383d86653367fc040644648791
zhanggongying <edsolater@qq.com>
2023-01-05 16:13:34 +0800

    🚧 use local calc ownerAllLossAmount

--- src/application/compensation/hydrateCompensationInfo.ts
+++ src/application/compensation/hydrateCompensationInfo.ts
@@ -1,5 +1,6 @@
 import { toTokenAmount } from '@/functions/format/toTokenAmount'
 import { isPubEqual } from '@/functions/judgers/areEqual'
+import { mul } from '@/functions/numberish/operations'
 import { SHOW_INFO } from '@raydium-io/raydium-sdk'
 import useLiquidity from '../liquidity/useLiquidity'
 import useToken from '../token/useToken'
@@ -11,23 +12,25 @@ export function hydrateNegativeMoneyInfo(raw: SHOW_INFO): HydratedCompensationIn
   const jsonInfo = jsonInfos.find((i) => isPubEqual(i.id, raw.ammId))
   const base = getToken(jsonInfo?.baseMint)
   const quote = getToken(jsonInfo?.quoteMint)
+  const snapshotLpAmount = getToken(jsonInfo?.lpMint)
+    ? toTokenAmount(getToken(jsonInfo?.lpMint)!, raw.snapshotLpAmount)
+    : undefined
   return {
     ...raw,
     rawInfo: raw,
     poolName: `${base?.symbol ?? ''}-${quote?.symbol ?? ''}`,
-    snapshotLpAmount: getToken(jsonInfo?.lpMint)
-      ? toTokenAmount(getToken(jsonInfo?.lpMint)!, raw.snapshotLpAmount)
-      : undefined,
+    snapshotLpAmount,
     openTime: new Date(raw.openTime * 1000),
     endTime: new Date(raw.endTime * 1000),
     tokenInfo: raw.tokenInfo.map((rawTokenInfo) => {
       const token = getToken(rawTokenInfo.mintAddress)
       if (!token) return undefined
+      const perLpLoss = toTokenAmount(token, rawTokenInfo.perLpLoss)
       return {
         ...rawTokenInfo,
         token,
-        perLpLoss: toTokenAmount(token, rawTokenInfo.perLpLoss),
-        ownerAllLossAmount: toTokenAmount(token, rawTokenInfo.ownerAllLossAmount),
+        perLpLoss,
+        ownerAllLossAmount: toTokenAmount(token, mul(snapshotLpAmount, perLpLoss), { alreadyDecimaled: true }),
         debtAmount: toTokenAmount(token, rawTokenInfo.debtAmount)
       }
     })
