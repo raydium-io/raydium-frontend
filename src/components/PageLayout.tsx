159baa548306076d4f04085903ed62b19922fbf7
zhanggongying <edsolater@qq.com>
2023-01-10 14:47:50 +0800

    🚑 V2.9.12 fix layout bug

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -118,7 +118,7 @@ export default function PageLayout(props: {
       )}
       <main
         // always occupy scrollbar space
-        className="PageLayoutContent relative grid-area-c bg-gradient-to-b from-[#0c0927] to-[#110d36] rounded-tl-3xl mobile:rounded-none"
+        className="flex flex-col PageLayoutContent relative grid-area-c bg-gradient-to-b from-[#0c0927] to-[#110d36] rounded-tl-3xl mobile:rounded-none"
         style={{
           overflowX: 'hidden',
           overflowY: 'scroll'
@@ -131,7 +131,7 @@ export default function PageLayout(props: {
         <DisclaimerDialog />
         <div
           className={twMerge(
-            `isolate flex-container p-12 ${
+            `grow h-0 isolate flex-container p-12 ${
               props.contentButtonPaddingShorter ?? props.contentYPaddingShorter ? 'pb-4' : ''
             } ${props.contentTopPaddingShorter ?? props.contentYPaddingShorter ? 'pt-5' : ''} mobile:py-2 mobile:px-3`,
             props.contentClassName
