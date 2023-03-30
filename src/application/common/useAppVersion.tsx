4c556888f85a7a29d9026358b8b10c462da1970e
zhanggongying <edsolater@qq.com>
2023-03-30 17:13:39 +0800

    ✨ v2.9.42 clmm start time

--- src/application/common/useAppVersion.tsx
+++ src/application/common/useAppVersion.tsx
@@ -8,7 +8,7 @@ import { inClient } from '@/functions/judgers/isSSR'
 import useAppAdvancedSettings from './useAppAdvancedSettings'
 
 // frontend (client)
-const APP_VERSION = 'V2.9.41'
+const APP_VERSION = 'V2.9.42'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
