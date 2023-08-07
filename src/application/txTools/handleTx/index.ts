77299f69038582693e918f60fdb884c068270601
Rudy <lengxin5348@gmail.com>
2023-08-07 22:11:55 +0800

    update

--- src/application/txTools/handleTx/index.ts
+++ src/application/txTools/handleTx/index.ts
@@ -1,13 +1,6 @@
 import { CacheLTA, InnerSimpleTransaction, TxVersion } from '@raydium-io/raydium-sdk'
 import {
-  Connection,
-  Context,
-  Keypair,
-  PublicKey,
-  SignatureResult,
-  Transaction,
-  TransactionError,
-  VersionedTransaction
+  Connection, Context, Keypair, PublicKey, SignatureResult, Transaction, TransactionError, VersionedTransaction
 } from '@solana/web3.js'
 
 import { produce } from 'immer'
@@ -77,11 +70,11 @@ export type TxSentErrorInfo = {
 
 export type TxFinalInfo =
   | ({
-      type: 'success'
-    } & TxSuccessInfo)
+    type: 'success'
+  } & TxSuccessInfo)
   | ({
-      type: 'error'
-    } & TxErrorInfo)
+    type: 'error'
+  } & TxErrorInfo)
 
 export type TxFinalBatchErrorInfo = {
   allSuccess: false
@@ -155,10 +148,10 @@ export type MultiTxsOption = {
    * send all at once
    */
   sendMode?:
-    | 'queue'
-    | 'queue(all-settle)'
-    | 'parallel(dangerous-without-order)' /* couldn't promise tx's order */
-    | 'parallel(batch-transactions)' /* it will in order */
+  | 'queue'
+  | 'queue(all-settle)'
+  | 'parallel(dangerous-without-order)' /* couldn't promise tx's order */
+  | 'parallel(batch-transactions)' /* it will in order */
 } & MultiTxCallbacks
 
 export type MultiTxCallbacks = {
@@ -297,7 +290,7 @@ export default async function txHandler(customizedTxAction: TxFn, options?: Hand
       singleTxOptions?.[0]?.txHistoryInfo?.forceErrorTitle ??
       (singleTxOptions?.[0]?.txHistoryInfo?.title ?? '') + ' Error' // assume first instruction's txHistoryInfo is same as the second one
     let systemErrorDescription = error instanceof Error ? noTailingPeriod(error.message) : String(error)
-    if (systemErrorDescription.includes("versioned transactions isn't supported")) {
+    if (systemErrorDescription.includes("versioned transactions isn't supported") || systemErrorDescription.includes("tx.serializeMessage")) {
       systemErrorDescription =
         'Transaction cancelled\nThis wallet might not support Versioned Transaction, turn it off and try again.'
     }
@@ -440,11 +433,11 @@ async function dealWithMultiTxOptions({
       try {
         const builded = transactions.every(isInnerTransaction)
           ? await buildTransactionsFromSDKInnerTransactions({
-              connection: payload.connection,
-              wallet: payload.owner,
-              txVersion: payload.txVersion,
-              transactions
-            })
+            connection: payload.connection,
+            wallet: payload.owner,
+            txVersion: payload.txVersion,
+            transactions
+          })
           : (transactions as Transaction[])
 
         try {
@@ -612,7 +605,7 @@ function composeWithDifferentSendMode({
           method: singleOption.continueWhenPreviousTx ?? (sendMode === 'queue(all-settle)' ? 'finally' : 'success')
         }
       },
-      { fn: () => {}, method: 'success' }
+      { fn: () => { }, method: 'success' }
     )
     return queued.fn
   }
