fcf96fb07b575d7ba841541d9463c2e4fe7be5df
zhanggongying <edsolater@qq.com>
2023-08-07 13:19:10 +0800

    💄 TxVersionWidget add tooltip

--- src/components/navWidgets/TxVersionWidget.tsx
+++ src/components/navWidgets/TxVersionWidget.tsx
@@ -2,13 +2,29 @@ import useWallet from '@/application/wallet/useWallet'
 import { TxVersion } from '@raydium-io/raydium-sdk'
 import Row from '../Row'
 import Switcher from '../Switcher'
+import Tooltip from '../Tooltip'
+import Icon from '../Icon'
+import useAppSettings from '@/application/common/useAppSettings'
 
 /** this should be used only in ./Navbar.tsx */
 export function TxVersionWidget() {
   const txVersion = useWallet((s) => s.txVersion)
+  const isMobile = useAppSettings((s) => s.isMobile)
   return (
-    <Row className="mobile:flex-col-reverse mobile:gap-0 items-center gap-2">
-      <div className="mobile:h-2 mobile:text-2xs text-sm text-[#abc4ff80]">ver.TX</div>
+    <Row className="mobile:flex-col-reverse mobile:translate-y-1 mobile:gap-0 items-center gap-2">
+      <Row className="items-center gap-1 text-[#abc4ff80]">
+        <div className="mobile:text-2xs text-sm">Ver.TX</div>
+        <Tooltip placement="bottom">
+          <Icon size={isMobile ? 'xs' : 'sm'} heroIconName="information-circle" />
+          <Tooltip.Panel>
+            <div className="max-w-[300px] space-y-1.5">
+              Versioned Tx provides more advanced routings and better prices. Current compatible wallets: Phantom,
+              Solflare, Glow and Backpack.
+            </div>
+          </Tooltip.Panel>
+        </Tooltip>
+      </Row>
+
       <Switcher
         // className="mobile:h-4"
         checked={txVersion === TxVersion.V0}
