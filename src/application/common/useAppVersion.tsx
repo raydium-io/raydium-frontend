4f23e84044b17489602dafedaabed7e4eaed8eee
zhanggongying <edsolater@qq.com>
2023-08-25 16:58:28 +0800

    🐛 V2.11.9 fix verify failed log twice

--- src/application/common/useAppVersion.tsx
+++ src/application/common/useAppVersion.tsx
@@ -8,7 +8,7 @@ import { inClient } from '@/functions/judgers/isSSR'
 import useAppAdvancedSettings from './useAppAdvancedSettings'
 
 // frontend (client)
-const APP_VERSION = 'V2.11.8'
+const APP_VERSION = 'V2.11.9'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
