2443c9eceed566bc60ba942090ac1133ed25f336
zhanggongying <edsolater@qq.com>
2023-09-18 13:32:24 +0800

    🐛 V2.11.15 toPub weakRef bug

--- src/application/common/useAppVersion.tsx
+++ src/application/common/useAppVersion.tsx
@@ -8,7 +8,7 @@ import { inClient } from '@/functions/judgers/isSSR'
 import useAppAdvancedSettings from './useAppAdvancedSettings'
 
 // frontend (client)
-const APP_VERSION = 'V2.11.14'
+const APP_VERSION = 'V2.11.15'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
