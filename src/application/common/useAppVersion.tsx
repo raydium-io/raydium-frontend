7682189e72c35d4c2feaba8b9cfbef69e8891a55
zhanggongying <edsolater@qq.com>
2023-11-20 14:30:37 +0800

    🐛 V2.11.25 fix pool list sort

--- src/application/common/useAppVersion.tsx
+++ src/application/common/useAppVersion.tsx
@@ -8,7 +8,7 @@ import { inClient } from '@/functions/judgers/isSSR'
 import useAppAdvancedSettings from './useAppAdvancedSettings'
 
 // frontend (client)
-const APP_VERSION = 'V2.11.24'
+const APP_VERSION = 'V2.11.25'
 
 const APP_VERSION_CHECKING_DELAY_TIME = 1000 * 60 * 1
 
