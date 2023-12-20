e3068164ed15f6c988461ccacf3b63efc8d469af
edsoalter <edsolater@qq.com>
2023-12-20 20:46:03 +0800

    🐛 owner change don't cause clmm position refetch

--- src/application/ammV3PoolInfoAndLiquidity/useAutoCleanLiquidityInfoCache.ts
+++ src/application/ammV3PoolInfoAndLiquidity/useAutoCleanLiquidityInfoCache.ts
@@ -3,14 +3,16 @@ import { clearSDKClmmPoolInfoCache } from '../common/getSDKParsedClmmPoolInfo'
 import { useIsomorphicLayoutEffect } from '@/hooks/useIsomorphicLayoutEffect'
 import useLiquidity from '../liquidity/useLiquidity'
 import { useSwap } from '../swap/useSwap'
+import useWallet from '../wallet/useWallet'
 
 // 💡 may belong to /model/ammAndLiquidity
 export default function useAutoCleanSwapInfoCache() {
+  const owner = useWallet((s) => s.owner)
   const swapRefresh = useSwap((s) => s.refreshCount)
   const liquidityRefresh = useLiquidity((s) => s.refreshCount)
 
   useIsomorphicLayoutEffect(() => {
     clearSDKCacheOfSwap()
     clearSDKClmmPoolInfoCache()
-  }, [swapRefresh, liquidityRefresh])
+  }, [swapRefresh, liquidityRefresh, owner])
 }
